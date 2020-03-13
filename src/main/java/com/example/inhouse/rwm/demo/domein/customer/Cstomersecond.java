8cbdbf47f3afc12e7fc6b71f140ea52140e76a87
AntonSergeichyk <sergeichykliii@gmail.com>
2020-03-13 21:31:18 +0300

    SBS0293434 added Customer model

---
+++ src/main/java/com/example/inhouse/rwm/demo/domein/customer/Cstomersecond.java
@@ -0,0 +1,11 @@
+package com.example.inhouse.rwm.demo.domein.customer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cstomersecond {
+
+    private String SecondName;
+}
